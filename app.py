24d8d8e273aa69ac700580b4cc31589fb5ec6396
Veronika Manko <manko.veronica@gmail.com>
2023-03-31 15:41:54 +0300

    initital commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__) # Створюємо веб–додаток Flask
+
+@app.route("/") # Вказуємо url-адресу для виклику функції
+def index():
+    return render_template("index.html")#Результат, що повертається у браузер
+
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(port=5001, debug=True) # Запускаємо веб-сервер з цього файлу
