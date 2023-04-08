5304ffe6d8db332ef2a6b162dd6c6f0d7085859c
Veronika Manko <manko.veronica@gmail.com>
2023-04-08 23:30:44 +0300

    hi

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__) # Створюємо веб–додаток Flask
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///portfolio.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(50), unique = True, nullable = False)
+    password = db.Column(db.String, nullable = False)
+    name = db.Column(db.String(200))
+    logiks = db.Column(db.Integer, default = 0)
+
+    def __repr__(self):
+        return f"User: {self.username}"
+
 
 @app.route("/") # Вказуємо url-адресу для виклику функції
 def index():
-    return render_template("index.html")#Результат, що повертається у браузер
+    users = User.query.all()
+    print(users)
+    return render_template("index.html", name = 'dolphin', users = users)# Результат, що повертається у браузер
+
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
---
+++ testdb.py
@@ -0,0 +1,8 @@
+# from app import *
+#
+# with app.app_context():
+#     db.create_all()
+#     user = User(name='Hacker', username='wfgwfg2ilgiwf', password='1234567890')
+#     user1 = User(name='Hello', username='whg72grfylufd2', password='1234')
+#     db.session.add_all([user, user1])
+#     db.session.commit()
